ff87f96ae0b90361da7c3018ac4749b0b8e3bf7e
Mateus Santos <mateus.freira@gmail.com>
2019-10-19 14:02:43 -0300

    Fix base UR

--- examples/todomvc/src/freira.js
+++ examples/todomvc/src/freira.js
@@ -2,4 +2,27 @@ import FreiraDb from 'freira-db';
 
 const freira = new FreiraDb('ws://45.56.110.92:3012');
 
-export default freira;
+let ignore = false;
+const dbMiddleware = store => {
+  freira.watch('lastEvent', action => {
+    ignore = true;
+    store.dispatch(action.value);
+  });
+  freira.getValue('lastState').then(state => {
+    ignore = true;
+    store.dispatch({ type: 'newState', state   });
+  });
+  return next => (action) => {
+    next(action);
+    if (!ignore) {
+      freira.setValue('lastEvent', action);
+      freira.setValue('lastState', store.getState());
+    }
+    ignore = false;
+  };
+};
+
+export {
+  freira,
+  dbMiddleware
+};
--- examples/todomvc/src/index.js
+++ examples/todomvc/src/index.js
@@ -1,49 +1,17 @@
 import React from 'react'
-import {
-  render
-} from 'react-dom'
-import {
-  createStore,
-  applyMiddleware
-} from 'redux'
-import {
-  Provider
-} from 'react-redux';
-import App from './components/App';
-import reducer from './reducers';
-import freira from './freira';
-import 'todomvc-app-css/index.css';
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
+import { dbMiddleware  } from './freira'
+import 'todomvc-app-css/index.css'
 
-let ignore = false;
-const customMiddleWare = store => {
-  freira.watch('lastEvent', action => {
-    ignore = true;
-    store.dispatch(action.value);
-  });
-  freira.getValue('lastState').then(state => {
-    ignore = true;
-    store.dispatch({ type: 'newState', state   });
-  });
-  return next => (action) => {
-    next(action);
-    if (!ignore) {
-      freira.setValue('lastEvent', action);
-      freira.setValue('lastState', store.getState());
-    }
-    ignore = false;
-  };
-};
+const store = createStore(reducer, applyMiddleware(dbMiddleware));
 
-const store = createStore(reducer, applyMiddleware(customMiddleWare));
-
-render( <
-  Provider store = {
-    store
-  } >
-  <
-  App / >
-  <
-  /Provider>,
+render(<Provider store ={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
 
