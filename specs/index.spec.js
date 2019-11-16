831dbe4f00ccff5e6324cad6b2aa9773b854ccf9
Mateus Santos <mateus.freira@gmail.com>
2019-11-16 08:08:31 -0300

    Add new tests and update package-lock.json¬

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -3,9 +3,38 @@ const freira = new FreiraDb('ws://45.56.110.92:3012');
 
 describe('Freira Db should', () => {
 
-  it('set value to a key', done => {
-    freira.getValue('some', Date.now()).then(done);
+  it('should set value to a key', () => {
+    const now = Date.now();
+    return freira.setValue('some', now).then(() => freira.getValue('some'))
+      .then(value => {
+        expect(value).to.be.equal(now);
+      });
   });
 
+  it('should watch a value', () => {
+    const now = Date.now();
+    const values = [];
+    const wait = time => {
+      return new Promise(resolve => {
+        setTimeout(resolve, time || 500);
+      });
+    };
+
+    const freira2 = new FreiraDb('ws://45.56.110.92:3012');
+    freira2.watch('some', ({
+      value
+    }) => {
+      values.push(value);
+    });
+    return wait().then(() => Promise.all([
+        freira.setValue('some', 1),
+        freira.setValue('some', 2),
+        freira.setValue('some', 3)
+      ]))
+      .then(() => wait()).then(() => {
+        expect(values.length).to.be.equals(3);
+        expect(values).to.be.deep.equals([1, 2, 3]);
+      });
+  });
 });
 
