d86af80e83421a2eadf28e46e2b81feb257cc1fc
Mateus Santos <mateus.freira@gmail.com>
2021-08-07 16:21:01 -0300

    make it all real time

--- examples/analitcs-real-time/analitcs.js
+++ examples/analitcs-real-time/analitcs.js
@@ -1,35 +1,43 @@
-window.onload = () => {
+const charts = {};
+
+function run() {
   performance.mark('keys-start');
   nun.keys()
     .then(keys => {
       performance.mark('keys-end');
       performance.measure('keysMarker', 'keys-start', 'keys-end');
-      performance.mark('load-start');
-      const data = {
-        users: {
-          keys: showUserData(keys.filter(u => u.startsWith("user_"))),
-        },
-        pages: buildAnalitcsData(keys, "page_").then(showPageData),
-        dates: buildAnalitcsData(keys.reverse(), "date_").then(showDateData),
-        languages: buildAnalitcsData(keys, "lang_").then(showLangData),
-        locations: buildAnalitcsData(keys, "location_").then(showLocationData),
-      };
-      return Promise.all([data.pages, data.dates, data.users, data.languages, data.locations]);
-    }).then(([_pagesData, _dateData, userData, _languageData, _locationData]) => {
-      performance.mark('load-end');
-      performance.measure('loadMarker', 'load-start', 'load-end');
+      showUserData(keys.filter(u => u.startsWith("user_")));
+      buildAnalitcsData(keys, "page_", showPageData);
+      buildAnalitcsData(keys.reverse(), "date_", showDateData);
+      buildAnalitcsData(keys, "lang_", showLangData);
+      buildAnalitcsData(keys, "location_", showLocationData);
+    }).then(() => {
     });
+}
+
+window.onload = () => {
+  run();
 };
 
 
-function buildAnalitcsData(allKeys, prefix) {
+function buildAnalitcsData(allKeys, prefix, plotFunction) {
   const keys = allKeys.filter(u => u.startsWith(prefix));
-  const valueChain = keys.map(key => nun.getValue(key).then((value) => ({
-    key,
-    value,
-    label: key.replace(prefix, "")
-  })));
-  return Promise.all(valueChain);
+  const finalObject = {};
+  let count = 0;
+  performance.mark(`${prefix}_start`);
+  keys.map(key => nun.watch(key, ({
+    value
+  }) => {
+    count++;
+    finalObject[key] = {
+      value,
+      key,
+      label: key.replace(prefix, "")
+    };
+    if (count >= keys.length)
+      plotFunction && plotFunction(Object.values(finalObject));
+  }, true));
+  return Promise.resolve([]);
 }
 
 function showUserData(userData) {
@@ -38,117 +46,142 @@ function showUserData(userData) {
 }
 
 function showDateData(_dateData) {
-  const dateData = _dateData.sort((p1, p2) => p1.key.localeCompare(p2.key));
-  const options = {
-    series: [{
-      name: "Page virews",
-      data: dateData.map(d => d.value),
-    }],
-    chart: {
-      height: 350,
-      type: 'line',
-      zoom: {
-        enabled: false
-      }
-    },
-    dataLabels: {
-      enabled: false
-    },
-    stroke: {
-      curve: 'straight'
-    },
-    title: {
-      text: 'Page virews by date',
-      align: 'left'
-    },
-    grid: {
-      row: {
-        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-        opacity: 0.5
-      },
-    },
-    xaxis: {
-      categories: dateData.map(d => d.label),
-    }
-  };
 
-  const chart = new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
+  const dateData = _dateData.sort((p1, p2) => p1.key.localeCompare(p2.key));
+  if (charts.dateChart) {
+    updateChart(charts.dateChart, dateData);
+  } else {
+    const options = {
+      series: [{
+        name: "Page virews",
+        data: dateData.map(d => d.value),
+      }],
+      chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      title: {
+        text: 'Page virews by date',
+        align: 'left'
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5
+        },
+      },
+      xaxis: {
+        categories: dateData.map(d => d.label),
+      }
+    };
+
+    const chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+    charts.dateChart = chart;
+  }
 }
 
-function showPageData(_pagesData) {
-  const pagesData = _pagesData.sort((p1, p2) => p2.value - p1.value);
-  const pageOptions = {
-    series: [{
-      name: 'Reads',
-      data: pagesData.map(p => p.value),
-    }],
-    annotations: {
-      points: [{
-        x: '/',
-        seriesIndex: 0,
-        label: {
-          borderColor: '#775DD0',
-          offsetY: 0,
-          style: {
-            color: '#fff',
-            background: '#775DD0',
-          },
-          text: 'Home page',
-        }
-      }]
-    },
-    chart: {
-      height: 350,
-      type: 'bar',
-    },
-    plotOptions: {
-      bar: {
-        borderRadius: 10,
-        columnWidth: '50%',
-      }
-    },
-    dataLabels: {
-      enabled: false
-    },
-    stroke: {
-      width: 2
-    },
-
-    grid: {
-      row: {
-        colors: ['#fff', '#f2f2f2']
-      }
-    },
+function updateChart(chart, data) {
+  chart.updateSeries([{
+    data: data.map(_ => _.value)
+  }]);
+  chart.updateOptions({
     xaxis: {
       labels: {
         rotate: -45
       },
-      categories: pagesData.map(p => p.label),
+      categories: data.map(p => p.label),
       tickPlacement: 'on'
     },
-    yaxis: {
-      title: {
-        text: 'Reads',
-      },
-    },
-    fill: {
-      type: 'gradient',
-      gradient: {
-        shade: 'light',
-        type: "horizontal",
-        shadeIntensity: 0.25,
-        gradientToColors: undefined,
-        inverseColors: true,
-        opacityFrom: 0.85,
-        opacityTo: 0.85,
-        stops: [50, 0, 100]
-      },
-    }
-  };
 
-  const pageChart = new ApexCharts(document.querySelector("#page-chart"), pageOptions);
-  pageChart.render();
+  });
+}
+
+function showPageData(_pagesData) {
+  const pagesData = _pagesData.sort((p1, p2) => p2.value - p1.value);
+  if (charts.pageChart) {
+    updateChart(charts.pageChart, pagesData);
+  } else {
+    const pageOptions = {
+      series: [{
+        name: 'Reads',
+        data: pagesData.map(p => p.value),
+      }],
+      annotations: {
+        points: [{
+          x: '/',
+          seriesIndex: 0,
+          label: {
+            borderColor: '#775DD0',
+            offsetY: 0,
+            style: {
+              color: '#fff',
+              background: '#775DD0',
+            },
+            text: 'Home page',
+          }
+        }]
+      },
+      chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 10,
+          columnWidth: '50%',
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        width: 2
+      },
+
+      grid: {
+        row: {
+          colors: ['#fff', '#f2f2f2']
+        }
+      },
+      xaxis: {
+        labels: {
+          rotate: -45
+        },
+        categories: pagesData.map(p => p.label),
+        tickPlacement: 'on'
+      },
+      yaxis: {
+        title: {
+          text: 'Reads',
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          type: "horizontal",
+          shadeIntensity: 0.25,
+          gradientToColors: undefined,
+          inverseColors: true,
+          opacityFrom: 0.85,
+          opacityTo: 0.85,
+          stops: [50, 0, 100]
+        },
+      }
+    };
+    charts.pageChart = new ApexCharts(document.querySelector("#page-chart"), pageOptions);
+    charts.pageChart.render();
+  }
   return pagesData;
 }
 
